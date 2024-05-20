ad643c145d8427759be2b34c7a5a62bc64838711
Kabin_giri <85396529+Kabin119245@users.noreply.github.com>
2024-05-20 20:51:14 +0545

    function and arithmetic

--- src/bin/1_comment.rs
+++ src/bin/1_comment.rs
@@ -1,3 +1,10 @@
+//this is the entry point of the appication
 fn main() {
+    //Display a message
     println!("Hello, world!");
+
+    //My favourite color
+    let _my_favourite_color = "red";
+   
+
 }
---
+++ src/bin/2_function.rs
@@ -0,0 +1,12 @@
+fn first_name() {
+    println!("Kabin")
+}
+
+fn last_name() {
+        println!("Giri")
+}
+
+fn main() {
+    first_name();
+    last_name();
+}
