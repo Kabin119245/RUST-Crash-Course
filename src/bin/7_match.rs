5adf5306400f5bc53583483f2a5dd5aabbacf10c
Kabin_giri <85396529+Kabin119245@users.noreply.github.com>
2024-05-20 21:33:37 +0545

    match expression

---
+++ src/bin/7_match.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let my_name = "Kabin";
+
+    match my_name {
+        "Kanchan" => println!("that is my girl's name"),
+        "Kabin"  => println!("Yup, thats me"),
+        "Kabina" => println!("she is my sister"),
+        _ => println!("I dont know you, Nice to meet you")
+     }
+}
