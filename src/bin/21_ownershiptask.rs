57abb287d58aca35197d3f21233f3976b9e41e69
Kabin_giri <85396529+Kabin119245@users.noreply.github.com>
2024-05-22 23:04:17 +0545

    task updated

--- src/bin/21_ownershiptask.rs
+++ src/bin/21_ownershiptask.rs
@@ -1,11 +1,26 @@
+struct GroceryItem {
+    quantity: i32,
+    id : i32,
+}
+
+fn print_quantity(item: &GroceryItem) {
+    println!("quantity is: {:?}", item.quantity);
+}
+
+fn print_id(item: &GroceryItem) {
+    println!("ID is: {:?}", item.id);
+}
 
 
+fn main() {
+
+    let gitem = GroceryItem {
+        quantity: 20,
+        id: 34,
+    };
+
+    print_quantity(&gitem);
+    print_id(&gitem);
 
 
-
-
-
-
-
-
-fn main() {}
+}
